cf2e4201f27b2ccb594b8cd8bbcbbe5e210b7d0e
agiha <agiha1028@gamil.com>
2022-04-14 16:11:34 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Layouts from './compoment/layout'
+
+function App() {
+  return (
+<Layouts></Layouts>
+  );
+}
+
+export default App;
